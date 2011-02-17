6ec1e878a9e71c341ee0f2b4079fa0a6ac38276c
FSX <frank@61924.nl>
2011-02-17 22:38:05 +0100

    Added .XZ.

--- unzipit/core.py
+++ unzipit/core.py
@@ -9,21 +9,23 @@ def main(fname, *test):
     exts = fname.split('.')[1:]
 
     flags = ['x']
-    
+
     if len(exts):
         if 'bz' in exts:
             flags.append('j')
-        if 'gz' in exts:
+        elif 'gz' in exts:
             flags.append('z')
-        
+        elif 'xz' in exts:
+            flags.append('J')
+
         print 'tar -%s %s' % (''.join(flags), fname)
-        
+
     else:
         print('Archive must have a recognizable extension.')
-    
-    
-    
-    
+
+
+
+
     # system('tar %s %s' % (flags, fname))
 
 def run():
