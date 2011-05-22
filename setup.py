fd1d675a1d6318963d3d363f19f69941cf30282a
Kenneth Reitz <me@kennethreitz.com>
2011-05-22 17:32:54 -0400

    update

--- setup.py
+++ setup.py
@@ -4,45 +4,47 @@
 import os
 import sys
 
-from distutils.core import setup
+from setuptools import setup
 
 
 def publish():
-"""Publish to PyPi"""
-os.system("python setup.py sdist upload")
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
 
-if sys.argv[-1] == "publish":
-publish()
-sys.exit()
+if __name__ == '__main__':
 
-required = []
+    if sys.argv[-1] == "publish":
+        publish()
+        sys.exit()
 
-setup(
-    name='unzipit',
-    version='0.0.1',
-    description='Helper command for unzipping files with tar.',
-    long_description=open('README.rst').read(),
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/unzipit',
-    packages= ['unzipit'],
-    install_requires=required,
-    license='ISC',
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3.0',
-        # 'Programming Language :: Python :: 3.1',
-    ),
-    entry_points = {
-        'console_scripts': [
-            'unzipit = unzipit.cli:main'
-        ],
-    }
-)
+    required = []
+
+    setup(
+        name='unzipit',
+        version='0.0.1',
+        description='Helper command for unzipping files with tar.',
+        long_description=open('README.rst').read(),
+        author='Kenneth Reitz',
+        author_email='me@kennethreitz.com',
+        url='https://github.com/kennethreitz/unzipit',
+        packages= ['unzipit'],
+        install_requires=required,
+        license='ISC',
+        classifiers=(
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'License :: OSI Approved :: ISC License (ISCL)',
+            'Programming Language :: Python',
+            # 'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            # 'Programming Language :: Python :: 3.0',
+            # 'Programming Language :: Python :: 3.1',
+        ),
+        entry_points = {
+            'console_scripts': [
+                'unzipit = unzipit.cli:main'
+            ],
+        }
+    )
--- unzipit/formats.py
+++ unzipit/formats.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
 
-exts = {
-    'tar.bz2':  ['tar -xj'],
-    'tar.gz':   ['tar -xz'],
-    'tar.7z':   ['7z x', 'tar -x'],
-    'tar.lzo':  ['tar -x --lzop'],
-    'tar.lzma': ['tar -x --lzma'],
-    'tar.lz':   ['tar -x -lzip'],
-    'tar.xz':   ['tar -x --xv'],
-    'tar.Z':    ['tar -xZ'],
-    'tar':      ['tar -x']
+expand_map = {
+    'tar.bz2':
+        ['tar xvjf {0}'],
+    'tar.gz':
+        ['tar xvzf {0}'],
+    'tgz':
+        ['tar xvzf {0}'],
+    'tar.7z':
+        ['7z x {0}', 'tar -x'],
+    'tar.lzo':
+        ['tar -x --lzop {0}'],
+    'tar.lzma':
+        ['tar -x --lzma {0}'],
+    'tar.lz':
+        ['tar -x -lzip {0}'],
+    'tar.xz':
+        ['tar -x --xv {0}'],
+    'tar.Z':
+        ['tar -xZ {0}'],
+    'tar':
+        ['tar xvf {0}'],
+    'zip':
+        ['unzip -u {0}'],
+    'egg':
+        ['unzip -u {0}'],
+    'jar':
+        ['unzip -u {0}'],
+    'gz':
+        ['gunzip {0}'],
+    'bz2':
+        ['bunzip2 {0}']
 }
+
+
+def filename_to_commands(fname):
+    """Returns a list of commands to expand a file."""
+
+    for (extension, command_chain) in expand_map.items():
+        if fname.endswith(extension):
+            return command_chain
